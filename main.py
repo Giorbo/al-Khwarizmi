2bf1d45b5a789780de2906e0634a5a3c5211dd5a
Giorgio Liberatore <43214182+Giorbo@users.noreply.github.com>
2018-12-06 15:18:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
-
-
-from list.DoubleLinkedList import ListaDoppiamenteCollegata as dLinkedList
+from list.LinkedList import ListaCollegata as linkedList
 from dictionary.dictTrees.avlTree import AVLTree as avl
 from dictionary.Dictionary import Dictionary
 
 
 class avlLinkedList(Dictionary):
-    def __init__(self, min, max, b):
-        assert (max > min), "max deve essere maggiore di min."
+    def __init__(self, minimo, massimo, b):
+        assert (massimo > minimo), "massimo deve essere maggiore di minimo."
         assert (b > 6), "b deve essere maggiore di 6."
-        assert ((abs(max - min)%b) == 0), "|max - min| deve essere multiplo di b."
+        assert ((abs(massimo - minimo)%b) == 0), "|massimo - minimo| deve essere multiplo di b."
 
-        self.min = min
-        self.max = max
+        self.minimo = minimo
+        self.massimo = massimo
         self.b = b
-        self.d = (max-min)/b
-        self.array = [None] * (d+2)
+        self.d = int((massimo-minimo)/b)
+        self.array = [None] * int((self.d+2))
 
-    def insert(self, key, value):
+    def insertArray(self, key, value):
         #index selection:
         #int i will be the index.
-        if (key < min):
+        if (key < self.minimo):
             i = self.d
-        elif (key >= max):
+        elif (key >= self.massimo):
             i = self.d + 1
         else:
             for j in range(self.d - 1):
-                if (self.min+(j*self.b)) <= inputKey < (self.min+j+1):
+                if (self.minimo+(j*self.b)) <= key < (self.minimo+(j+1)*self.b):
                     i = j
-                    
-                    
+		#check if array[i] is avlTree or linkedList
         if type(self.array[i])=='dictionary.dictTrees.avlTree':
-				do something
-			else:
-				if self.array[i] == None:
-					crea lista
-				else:
-					aggiungi alla lista 
+				#self.array[i].self.avl.insert(key, value)
+            pass
+        else:
+            if self.array[i] == None:
+                self.array[i] = linkedList()
+                self.array[i].addAsFirst((key,value))
+            else:
+                self.array[i].addAsLast((key,value))
