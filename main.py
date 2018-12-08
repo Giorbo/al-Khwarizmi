0060400091b2162157a5a09e2f7670a292b737b7
Francesco <45074171+francescolasco@users.noreply.github.com>
2018-12-08 12:07:48 +0100

    Update main.py
    changelog:
    aggiunto e testato il modulo 'delete'.
    aggiunta la funzione ausiliaria getIndex.
    spostata la vecchia main in mainOld.py

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from dictionary.linkedListDictionary import LinkedListDictionary as linkedList
 from dictionary.dictTrees.avlTree import AVLTree as avl
 from dictionary.Dictionary import Dictionary
 
+# Functions:
+
+def getIndex(massimo, minimo, b, d, key):
+    if (key < minimo):
+        i = d
+    elif (key >= massimo):
+        i = d + 1
+    else:
+        for j in range(d - 1):
+            if (minimo+(j*b)) <= key < (minimo+(j+1)*b):
+                i = j
+    return i
+
 
 class avlLinkedList(Dictionary):
     def __init__(self, minimo, massimo, b):
@@ -20,32 +33,39 @@ class avlLinkedList(Dictionary):
     def insert(self, key, value):
         #index selection:
         #int i will be the index.
-        if (key < self.minimo):
-            i = self.d
-        elif (key >= self.massimo):
-            i = self.d + 1
-        else:
-            for j in range(self.d - 1):
-                if (self.minimo+(j*self.b)) <= key < (self.minimo+(j+1)*self.b):
-                    i = j
+        i = getIndex(self.massimo, self.minimo, self.b, self.d, key)
+
 		#check if array[i] is avlTree or linkedList
         if isinstance(self.array[i], avl):
-            print('spippettone')
             self.array[i].insert(key, value)
             self.counterList[i] += 1
         else:
             if self.array[i] == None:
-                print('ye')
                 self.array[i] = linkedList()
                 self.array[i].insert(key,value)
                 self.counterList[i] += 1
             else:
-                print('dynamint')
                 self.array[i].insert(key,value)
                 self.counterList[i] += 1
-                #controllo se la lista ha più di r elementi
+                #check if counterList has more than r elements
                 if self.counterList[i] >= self.r:
                     self.__linkedListToAvl(i)
+
+    def delete(self, key):
+        #index selection:
+        #int i will be the index.
+        i = getIndex(self.massimo, self.minimo, self.b, self.d, key)
+
+        if isinstance(self.array[i], avl):
+            self.array[i].delete(key)
+            self.counterList[i] -= 1
+            #check if counterList has more than r elements
+            if self.counterList[i] < self.r:
+                self.__avlToLinkedList(i)
+        else:
+            assert (self.array[i].theList.first != None), "la LinkedList al posto i e' gia' vuota."
+            self.array[i].delete(key)
+            self.counterList[i] -= 1
 
 
     def __linkedListToAvl(self, index):
@@ -55,3 +75,12 @@ class avlLinkedList(Dictionary):
             tempAvl.insert(current.elem[0], current.elem[1])
             current = current.next
         self.array[index] = tempAvl
+
+    def __avlToLinkedList(self, index):
+        tempLinkedList = linkedList()
+        for i in range(self.counterList[index]):
+            rootKey = self.array[index].tree.root.info[0]
+            rootValue = self.array[index].tree.root.info[1]
+            tempLinkedList.insert(rootKey, rootValue)
+            self.array[index].delete(rootKey)
+        self.array[index] = tempLinkedList
