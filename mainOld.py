19843a874d28579396a23033247a7a9dfdae238f
Francesco <francescolasco@gmail.com>
2018-12-11 12:49:09 +0100

    update

--- mainOld.py
+++ mainOld.py
@@ -1,8 +1,19 @@
-#old version.
-
 from dictionary.linkedListDictionary import LinkedListDictionary as linkedList
 from dictionary.dictTrees.avlTree import AVLTree as avl
 from dictionary.Dictionary import Dictionary
+
+# Functions:
+
+def getIndex(massimo, minimo, b, d, key):
+    if (key < minimo):
+        i = d
+    elif (key >= massimo):
+        i = d + 1
+    else:
+        for j in range(d - 1):
+            if (minimo+(j*b)) <= key < (minimo+(j+1)*b):
+                i = j
+    return i
 
 
 class avlLinkedList(Dictionary):
@@ -22,32 +33,35 @@ class avlLinkedList(Dictionary):
     def insert(self, key, value):
         #index selection:
         #int i will be the index.
-        if (key < self.minimo):
-            i = self.d
-        elif (key >= self.massimo):
-            i = self.d + 1
+        i = getIndex(self.massimo, self.minimo, self.b, self.d, key)
+
+        if self.array[i] == None:
+        	self.array[i] = linkedList()
+        self.array[i].insert(key, value)
+        self.counterList[i] += 1
+        if self.counterList[i] == self.r:
+            self.__linkedListToAvl(i)
+
+    def delete(self, key):
+        #index selection:
+        #int i will be the index.
+        i = getIndex(self.massimo, self.minimo, self.b, self.d, key)
+        assert(self.array[i] != None), "key not in data structure."
+        if self.counterList[i] == 1:
+            self.array[i] = None
+            self.counterList[i] = 0
         else:
-            for j in range(self.d - 1):
-                if (self.minimo+(j*self.b)) <= key < (self.minimo+(j+1)*self.b):
-                    i = j
-		#check if array[i] is avlTree or linkedList
-        if isinstance(self.array[i], avl):
-            print('spippettone')
-            self.array[i].insert(key, value)
-            self.counterList[i] += 1
-        else:
-            if self.array[i] == None:
-                print('ye')
-                self.array[i] = linkedList()
-                self.array[i].insert(key,value)
-                self.counterList[i] += 1
-            else:
-                print('dynamint')
-                self.array[i].insert(key,value)
-                self.counterList[i] += 1
-                #controllo se la lista ha più di r elementi
-                if self.counterList[i] >= self.r:
-                    self.__linkedListToAvl(i)
+            self.array[i].delete(key)
+            self.counterList[i] -= 1
+
+            if self.counterList[i] == self.r - 1:
+                self.__avlToLinkedList(i)
+
+
+    def search(self, key):
+        i = getIndex(self.massimo, self.minimo, self.b, self.d, key)
+        assert(isinstance(self.array[i], linkedList) or isinstance(self.array[i], avl)),"Error, the selected item can't be found in an existing set"
+        return self.array[i].search(key)
 
 
     def __linkedListToAvl(self, index):
@@ -57,3 +71,13 @@ class avlLinkedList(Dictionary):
             tempAvl.insert(current.elem[0], current.elem[1])
             current = current.next
         self.array[index] = tempAvl
+
+    def __avlToLinkedList(self, index):
+        tempLinkedList = linkedList()
+        for i in range(self.counterList[index]):
+            rootKey = self.array[index].tree.root.info[0]
+            rootValue = self.array[index].tree.root.info[1]
+            tempLinkedList.insert(rootKey, rootValue)
+            self.array[index].delete(rootKey)
+        self.array[index] = tempLinkedList
+
